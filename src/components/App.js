6be6df295b2e7abb064edf7fcd7bcd5f553e1100
Mohd Faizan <gitfaizan0496@gmail.com>
2023-06-03 01:18:33 +0530

    remove function App components into class components

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      albums: [],
+      updateAlbum: {}
+    }
+  }
+ render(){ return (
+  
+ )
+}
+}
+
+export default App;
